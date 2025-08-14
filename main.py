f8048446083cec881b00476eda6dee35d261d91e
dayto <virtu0427@gmail.com>
2025-08-14 15:23:24 +0900

    조대인 Selection Update

--- main.py
+++ main.py
@@ -6,22 +6,20 @@ MAX_N = 10_000
 
 def merge_sort():
 
-def selection_sort():
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
 def quick_sort():
 
 def bubble_sort():
 
 def insertion_sort(arr):
-    n = len(arr)
-    for i in range(1, n):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-
 
 def heap_sort():
     
